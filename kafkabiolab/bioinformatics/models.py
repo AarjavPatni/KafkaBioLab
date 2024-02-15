c7fc43a6f77072caf7be6cc5a1ee9233dee127e9
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-02-15 01:19:46 -0500

    Add Sequence model to bioinformatics module

--- kafkabiolab/bioinformatics/models.py
+++ kafkabiolab/bioinformatics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from uuid import uuid4
+
 
 # Create your models here.
+class Sequence(models.model):
+    input_sequence = models.CharField(max_length=10000)
+    operation = (
+        models.integerField()
+    )  # 1 - reverse, 2 - complement, 3 - reverse complement
+    output_sequence = models.CharField(max_length=10000)
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
